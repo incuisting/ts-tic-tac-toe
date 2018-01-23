9fc0ef377c2ad0b4572d024f71d89dc63cd09936
xuyanting <393146040@qq.com>
2018-01-23 14:31:51 +0800

    添加styledc-components的支持

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,14 +1,37 @@
 import * as React from 'react';
+import styled from 'styled-components';
 
 interface Props { // 定义prop的接口
     value: string | null; // 定义传入value的类型 为一个Union Types
     onClick: () => void;
+    className?: string;
 }
 
-export function Square(props: Props) {
+function SquareBody(props: Props) {
     return (
-        <button className="square" onClick={props.onClick}>
+        <button className={props.className} onClick={props.onClick}>
             {props.value}
         </button>
     );
 }
+
+export const Square = styled(SquareBody) `
+    background: #fff;
+    border: 1px solid #999;
+    float: left;
+    font-size: 24px;
+    font-weight: bold;
+    line-height: 34px;
+    height: 34px;
+    margin-right: -1px;
+    margin-top: -1px;
+    padding: 0;
+    text-align: center;
+    width: 34px;
+    &:focus{
+        outline: none;
+    }
+    &:active{
+        background: #ddd;        
+    }
+`;
