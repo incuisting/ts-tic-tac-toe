30c51d8851d042e1b89fde4b8a37fbbeabe8eefb
xuyanting <393146040@qq.com>
2018-01-30 11:26:50 +0800

    readonly

--- src/reducer/game.tsx
+++ src/reducer/game.tsx
@@ -2,10 +2,11 @@ import { handleActions } from 'redux-actions';
 // import { HandleClick, JumpTo } from '../actions/gameActions';
 import { History, StepNumber, XIsNext } from '../actions/gameActions';
 
+// readonly 可以使得代码在编译时和编写时 该状态是只读的，但是一旦编译之后就没有作用了  
 export interface GameState {
-    history: History;
-    stepNumber: StepNumber;
-    xIsNext: XIsNext;
+    readonly history: History;
+    readonly stepNumber: StepNumber;
+    readonly xIsNext: XIsNext;
 }
 const defaultState = {
     history: [{
--- src/store.tsx
+++ src/store.tsx
@@ -1,8 +1,6 @@
 import { applyMiddleware , createStore } from 'redux';
 import rootReducer from './reducer/index';
 import { RootState } from './store/rootState';
-// import { StoreState } from './types/index';
-// import { GameProps } from './containers/Game';
 import logger from 'redux-logger';
 
 const store = createStore<RootState>(
